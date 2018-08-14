c408b675d27b92aa862e2a4594c986e4fd15aaaf
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-08-14 13:17:31 -0500

    Push

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidth = 16f;
+
+    private void Update()
+    {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidth;
+        Debug.Log(Screen.width);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
