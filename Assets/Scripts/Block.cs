c2da41bb89423aec65746a2d25a4b285bd83ad8e
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-12-10 02:16:50 -0600

    Particles, changed prefabs, fixed bugs and minor changes.
    Imported particle system and changed block script.  Did minor things with tags in unity but still need to finish.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,6 +7,7 @@ public class Block : MonoBehaviour
 {
     
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
     Level level;
     GameSession gamestatus;
     private void Start()
@@ -21,11 +22,25 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
+        PlayBlockDestroyedSFX();
+        Destroy(gameObject);
+        TriggerParticles();
+        level.BlocksDestroyed();
+
+    }
+
+    private void PlayBlockDestroyedSFX()
+    {
         FindObjectOfType<GameSession>().AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(gameObject);
-        level.BlocksDestroyed();
-        
-        
+    }
+
+    private void TriggerParticles()
+    {
+        if (tag == "Breakable")
+        {
+            GameObject sparkes = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+            Destroy(sparkes, 1f);
+        }
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -27,5 +27,6 @@ public class Level : MonoBehaviour
             sceneloader.LoadNextScene();
 
         }
+     
     }
 }
